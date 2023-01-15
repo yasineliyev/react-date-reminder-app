9de4e05d63184e2f438ef075c9a5f47218b7d3e3
Yasin <105815296+yasineliyev@users.noreply.github.com>
2023-01-15 23:40:31 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { data } from "./data";
 
 function App() {
+  const [holidays, setHolidays] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>National Holidays</h3>
+        {holidays.map((holiday) => {
+          return (
+            <div className="holiday" key={holiday.id}>
+              <img src={holiday.img} alt="holiday" />
+              <div>
+                <p>{holiday.title}</p>
+                <p>{holiday.date}</p>
+              </div>
+            </div>
+          );
+        })}
+        {holidays === data ? (
+          <button onClick={() => setHolidays([])}>Clear All</button>
+        ) : (
+          <button onClick={() => setHolidays(data)}>Show All</button>
+        )}
+      </section>
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    id: 1,
+    title: "Müstəqillik Günü",
+    img: "https://525.az/img/pics/large/2021-05/192828_q5wsp56b5v.jpg",
+    date: "28 May",
+  },
+  {
+    id: 2,
+    title: "Azərbaycan Respublikası Silahlı Qüvvələri günü",
+    img: "https://mod.gov.az//images/gallery/429d2839841257d408730b2caecc542b.jpg",
+    date: "26 İyun",
+  },
+  {
+    id: 3,
+    title: "Zəfər Günü",
+    img: "https://i.ytimg.com/vi/UDZNZRfvbM4/maxresdefault.jpg",
+    date: "8 Noyabr",
+  },
+  {
+    id: 4,
+    title: "Dünya azərbaycanlılarının həmrəyliyi günü",
+    img: "https://hospital.avrasiya.az/images/news_images/201_view.jpg",
+    date: "31 dekabr",
+  },
+];
